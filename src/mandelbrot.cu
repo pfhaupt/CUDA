023f9edd7a7a5d0f84a3afe08513ce75c47e540d
Iselgrey <17763171+Iselgrey@users.noreply.github.com>
2025-03-11 17:23:20 +0530

    added basic implementation of double precision dekker algorithm to `src/mandelbrot.cu`. This is to prove nvidia native `double` is bad and they should feel bad.

--- src/mandelbrot.cu
+++ src/mandelbrot.cu
@@ -10,6 +10,168 @@
 
 typedef double gpuType;
 
+// #define USE_DOUBLE_DEKKER
+
+
+/* BEGIN DOUBLE IT LIKE DEKKER */
+// Source from Creel yt channel videos:
+//  https://www.youtube.com/watch?v=6OuqnaHHUG8
+//  https://www.youtube.com/watch?v=5IL1LJ5noww
+// adapted to go from float to double precision
+
+typedef struct{
+    float upper;
+    float lower;
+}DD;
+
+// SCALE is 2^(mantissabits/2)+1 = 2^(24/2)+1 = 4097
+const float SCALE = 4097;
+
+// float -> DD
+__device__ DD dd_split(float a){
+    DD R;
+    float p = a*SCALE;
+    R.upper = (a-p)+p;
+    R.lower = a-R.upper;
+    return R;
+}
+
+// DD -> float (loss of precision)
+__device__ float dd_join(DD a){
+    return a.upper+a.lower; //TODO: is this really it?
+}
+
+//  DD + DD -> DD
+__device__ DD dd_add(const DD a, const DD b){
+    DD r;
+    r.upper = a.upper+b.upper;
+    r.lower = 0;
+
+    if(abs(a.upper)>abs(b.upper)){
+        r.lower = a.upper-r.upper+b.upper+b.lower+a.lower;
+    }else{
+        r.lower = b.upper-r.upper+a.upper+a.lower+b.lower;
+    }
+
+    DD c;
+    c.upper = r.upper+r.lower;
+    c.lower = r.upper-c.upper+r.lower;
+
+    return c;
+}
+
+// DD - DD -> DD
+__device__ DD dd_sub(const DD a, const DD b){
+    DD r;
+    r.upper = a.upper-b.upper;
+    r.lower = 0;
+
+    if(abs(a.upper)>abs(b.upper)){
+        r.lower = a.upper-r.upper-b.upper-b.lower+a.lower;
+    }else{
+        r.lower = -b.upper-r.upper+a.upper+a.lower-b.lower;
+    }
+
+    DD c;
+    c.upper = r.upper+r.lower;
+    c.lower = r.upper-c.upper+r.lower;
+
+    return c;
+}
+
+// float * float -> DD
+__device__ DD dd_mul12(float a, float b){
+    DD A = dd_split(a);
+    DD B = dd_split(b);
+
+    float p = A.upper*B.upper;
+    float q = A.upper*B.lower+A.lower*B.upper;
+
+    DD R;
+    R.upper = p+q;
+    R.lower = p-R.upper+q+A.lower*B.lower;
+
+    return R;
+}
+
+// DD * DD -> DD
+__device__ DD dd_mul(const DD a, const DD b){
+    DD t = dd_mul12(a.upper,b.upper);
+    float c = a.upper*b.lower+a.lower*b.upper + t.lower;
+
+    DD r;
+    r.upper = t.upper+c;
+    r.lower = t.upper-r.upper+c;
+
+    return r;
+}
+
+// DD / DD -> DD (a/b)
+__device__ DD dd_div(const DD a, const DD b){
+    DD u;
+    u.upper = a.upper/b.upper;
+    DD t = dd_mul12(u.upper,b.upper);
+
+    /* float l = (a.upper-t.upper-t.lower+n.lower-u.upper*b.lower)/b.upper; */
+    float l = ((((a.upper-t.upper)-t.lower)+a.lower)-u.upper*b.lower)/b.upper;
+
+    DD r;
+    r.upper = u.upper+l;
+    r.lower = u.upper-r.upper+l;
+
+    return r;
+}
+
+
+/* END OF DOUBLE IT LIKE DEKKER */
+
+
+
+__device__ int compute_dd(gpuType *info, int id, int width, int height) {
+    DD screenMiddleX = dd_split(info[0]);
+    DD screenMiddleY = dd_split(info[1]);
+    DD screenW = dd_split(info[2]);
+    DD screenH = dd_split(info[3]);
+    DD dd2 = dd_split(2.0);
+    int maxIter = cast(info[4], int);
+    DD screenX = dd_sub(screenMiddleX, dd_div(screenW , dd2));
+    DD screenY = dd_sub(screenMiddleY, dd_div(screenH , dd2));
+    int _x = id % width;
+    int _y = id / width;
+    DD ddwidth = dd_split(width);
+    DD ddheight = dd_split(height);
+    DD percX = dd_div(dd_split(_x), ddwidth);
+    DD percY = dd_div(dd_split(_y), ddheight);
+    DD posx, posy;
+    if (width < height) {
+        posx = dd_add(screenX , dd_mul(percX,screenW));
+        posy = dd_mul(dd_add(screenY , dd_mul(percY,screenH)) , dd_div(ddheight,ddwidth));
+    } else {
+        posx = dd_mul(dd_add(screenX , dd_mul(percX,screenW)) , dd_div(ddwidth,ddheight));
+        posy = dd_add(screenY , dd_mul(percY,screenH));
+    }
+    int i = 0;
+    DD zx = posx;
+    DD zy = posy;
+    DD zrsqr = dd_mul(zx,zx);
+    DD zisqr = dd_mul(zy,zy);
+    DD a = dd_add(zrsqr,zisqr);
+    while (i < maxIter && dd_join(a) < 256) {
+        a = dd_add(zrsqr,zisqr);
+        zy = dd_mul(zx,zy);
+        zy = dd_add(zy,zy);
+        zy = dd_add(zy,posy);
+        zx = dd_add(dd_sub(zrsqr,zisqr),posx);
+        zrsqr = dd_mul(zx,zx);
+        zisqr = dd_mul(zy,zy);
+        i++;
+    }
+    if (i == maxIter) return 0;
+    return i;
+}
+
+
+
 __device__ int compute(gpuType *info, int id, int width, int height) {
     gpuType screenMiddleX = info[0];
     gpuType screenMiddleY = info[1];
@@ -63,7 +225,11 @@ __global__ void mandel(gpuType *screen, int *pixels, int width, int height) {
     const int N = width * height;
     for (int id = blockIdx.x * blockDim.x + threadIdx.x;
         id < N; id += blockDim.x * gridDim.x) {
+#ifndef USE_DOUBLE_DEKKER
         int iter = compute(screen, id, width, height);
+#else
+        int iter = compute_dd(screen, id, width, height);
+#endif
         int col = color(screen, iter);
         pixels[id] = col;
     }
@@ -95,6 +261,7 @@ int main() {
     copyHostToDevice(d_screen, info, infoSize);
 
     invokeKernel(mandel, 4096, 1024, d_screen, d_pixels, WIDTH, HEIGHT);
+    ENSURE(cudaStreamSynchronize(0), "First pass failed");
     copyDeviceToHost(pixels, d_pixels, WIDTH * HEIGHT);
     Image img = {
         pixels,
